ed6d981e523342b96c91810b4e1b3363888c943d
llshl <lsh970708@naver.com>
2021-08-02 01:43:40 +0900

    Initial commit

---
+++ src/main/java/com/example/jwtsecurity/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.example.jwtsecurity.controller;
+
+
+import com.example.jwtsecurity.entity.User;
+import com.example.jwtsecurity.jwt.JwtTokenProvider;
+import com.example.jwtsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> user) {
+        return userRepository.save(User.builder()
+                .email(user.get("email"))
+                .password(passwordEncoder.encode(user.get("password")))
+                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .build()).getId();
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user) {
+        User member = userRepository.findByEmail(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+    }
+
+    @PostMapping("/user/test")
+    public Map userResponseTest() {
+        Map<String, String> result = new HashMap<>();
+        result.put("result","user ok");
+        return result;
+    }
+
+    @PostMapping("/admin/test")
+    public Map adminResponseTest() {
+        Map<String, String> result = new HashMap<>();
+        result.put("result","admin ok");
+        return result;
+    }
+}
---
+++ src/main/java/com/example/jwtsecurity/service/CustomUserDetatilService.java
@@ -0,0 +1,21 @@
+package com.example.jwtsecurity.service;
+
+import com.example.jwtsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetatilService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
