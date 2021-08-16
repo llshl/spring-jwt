9c7a7f182326e9eb1abc37fd421cc90f454ac281
llshl <lsh970708@naver.com>
2021-08-16 21:51:36 +0900

    feat: jwt 인터셉터

---
+++ src/main/java/com/example/jwtsecurity/interceptor/JwtTokenInterceptor.java
@@ -0,0 +1,37 @@
+package com.example.jwtsecurity.interceptor;
+
+import com.example.jwtsecurity.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+
+        System.out.println("JwtToken 호출");
+        String accessToken = request.getHeader("ACCESS_TOKEN");
+        System.out.println("AccessToken:" + accessToken);
+        String refreshToken = request.getHeader("REFRESH_TOKEN");
+        System.out.println("RefreshToken:" + refreshToken);
+
+        if (accessToken != null && jwtTokenProvider.isValidAccessToken(accessToken)) {
+            return true;
+        }
+
+        response.setStatus(401);
+        response.setHeader("ACCESS_TOKEN", accessToken);
+        response.setHeader("REFRESH_TOKEN", refreshToken);
+        response.setHeader("msg", "Check the tokens.");
+        return false;
+    }
+}
