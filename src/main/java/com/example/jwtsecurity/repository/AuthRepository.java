6918f40c9230948d89aeaeaa8939205dba9f4e1e
llshl <lsh970708@naver.com>
2021-08-16 21:51:14 +0900

    feat: repository 추가

--- src/main/java/com/example/jwtsecurity/repository/AuthRepository.java
+++ src/main/java/com/example/jwtsecurity/repository/AuthRepository.java
@@ -1,10 +1,11 @@
 package com.example.jwtsecurity.repository;
 
+import com.example.jwtsecurity.entity.Auth;
 import com.example.jwtsecurity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserId(String userId);
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+    Optional<Auth> findByUserId(Long userId);
 }
